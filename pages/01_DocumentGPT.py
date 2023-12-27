9d0b53597cad8bcd758b50ca7ee28f71572e80e9
wish-mobile <conan@wish-mobile.com>
2023-12-27 23:51:03 +0900

    DocumentGPT 문서 업로드

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,34 +1,51 @@
 import streamlit as st
 import time
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.storage import LocalFileStore
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="😀",
 )
 
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    # st.write(file_content, file_path)
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    spliter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator= "\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader("./files/chapter_one.txt")
+    docs = loader.load_and_split(text_splitter=spliter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings =  CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir
+    )
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+    Welcome!
 
+    Use this chatbot to ask questions to an AI about your files!
+"""
+)
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-        if save:
-            st.session_state["messages"].append({"message": message, "role": role})
+file = st.file_uploader("Upload a .txt .pdf or .docx file",type=["pdf","txt","docx"])
 
-
-for message in st.session_state["messages"]:
-    send_message(
-        message["message"],
-        message["role"],
-        save=False,
-    )
-
-message = st.chat_input("Send a message to AI")
-
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
+if(file):
+    retriever = embed_file(file)
+    s = retriever.invoke("Winston")
+    s
