2cfd03868a3157e1d17602ecbd55a691012b1944
wish-mobile <conan@wish-mobile.com>
2023-12-27 16:31:13 +0900

    Streamlit 여러 위젯들

--- Home.py
+++ Home.py
@@ -1,12 +1,26 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
+from datetime import datetime
 
-a = [1,2,3,4]
+today = datetime.today().strftime("%H:%M:%S")
 
-d = {"x":1}
+st.title(today)
 
-a
+model = st.selectbox(
+    "Choose your model",
+    ("GPT-3", "GPT-4"),
+)
 
-d
+if model == "GPT-3":
+    print("cheap")
+else:
+    print("not cheap")
+    name = st.text_input("What is your name?")
+    st.write(name)
 
-st.selectbox("Choose your model", ("GPT-3","GPT-4"),)
+    value = st.slider(
+        "temperature",
+        min_value=0.1,
+        max_value=1.0,
+    )
+
+    st.write(value)
